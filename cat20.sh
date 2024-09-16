267389b94346bccdae76b23e405f3a4a75dfa1d4
Kaindotk <90937944+Kaindotk@users.noreply.github.com>
2024-09-16 22:05:40 +0300

    Update cat20.sh

--- cat20.sh
+++ cat20.sh
@@ -38,9 +38,9 @@ install_env_and_full_node() {
 
     cd ../../
     sudo docker build -t tracker:latest .
-    curl -O http://88.99.70.27:41187/dump_file_14635.sql
+    curl -O http://88.99.70.27:41187/dump_file_15644.sql
     sudo apt-get install postgresql-client
-    psql -h 127.0.0.1 -U postgres -d postgres -f dump_file_14635.sql
+    psql -h 127.0.0.1 -U postgres -d postgres -f dump_file_15644.sql
     sudo docker run -d \
         --name tracker \
         --add-host="host.docker.internal:host-gateway" \
