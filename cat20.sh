7dbffbb360046512ac1b48869d845f0e2eda82fb
Kaindotk <90937944+Kaindotk@users.noreply.github.com>
2024-09-23 20:44:20 +0300

    Update cat20.sh

--- cat20.sh
+++ cat20.sh
@@ -14,19 +14,8 @@ check_root() {
 install_env_and_full_node() {
     check_root
     sudo apt update && sudo apt upgrade -y
-    sudo apt install git
-    sudo apt-get install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt install git curl -y
+    sudo snap install docker -y
 
     sudo apt-get install npm -y
     sudo npm install n -g
